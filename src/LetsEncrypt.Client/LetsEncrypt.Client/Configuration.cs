c8d36294506f66a3bf18bbf5955dbeb8c9f1d80f
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2019-07-10 12:36:01 +0200

    Initial commit

---
+++ src/LetsEncrypt.Client/LetsEncrypt.Client/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LetsEncrypt.Client
+{
+    public class Configuration
+    {
+        public Boolean IsStaging { get; set; }
+        public String AccountEmail { get; set; }
+        public String AccountPem { get; set; }
+        public String DomainName { get; set; }
+        public String Country { get; set; }
+        public String State { get; set; }
+        public String Locality { get; set; }
+        public String Organization { get; set; }
+        public String Unit { get; set; }
+        public String CertificatePassword { get; set; }
+        public Int32 Delay { get; set; }
+        public String ChallengePath { get; set; }
+        public String CertificatePath { get; set; }
+    }
+}
---
+++ src/LetsEncrypt.Client/LetsEncrypt.Client/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Certes;
+using Certes.Acme;
+using Certes.Acme.Resource;
+using static System.Console;
+
+namespace LetsEncrypt.Client
+{
+    class Program
+    {
+        private static Configuration _configuration = new Configuration();
+
+        static async Task Main(string[] args)
+        {
+            ShowTitle();
+            LoadConfiguration();
+
+            while (true)
+            {
+                try
+                {
+                    if (CertificateIsAboutToExpire())
+                    {
+                        var acme = await LoadAccount();
+                        var order = await CreateOrder(acme);
+                        await ValidateOrder(order);
+                        await GenerateOrder(order);
+                    }
+                    else
+                    {
+                        Log(" V. Current certificate is still valid :)");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log($" X. ERROR {ex}");
+                }
+
+                await Task.Delay(60000);
+            }
+        }
+
+        private static bool CertificateIsAboutToExpire()
+        {
+            var certPath = Path.Combine(_configuration.CertificatePath, $"{_configuration.DomainName}.pfx");
+            var certPass = _configuration.CertificatePassword;
+
+            if (!File.Exists(certPath))
+            {
+                return true;
+            }
+
+            var isAboutToExpire = false;
+
+            X509Certificate2Collection collection = new X509Certificate2Collection();
+            collection.Import(certPath, certPass, X509KeyStorageFlags.PersistKeySet);
+            foreach (var cert in collection)
+            {
+                isAboutToExpire = isAboutToExpire || cert.NotAfter < DateTime.Today.AddDays(7);
+            }
+
+            return isAboutToExpire;
+        }
+
+        private static async Task GenerateOrder(IOrderContext order)
+        {
+            Log(" 7. Generating Certificate...");
+            var privateKey = KeyFactory.NewKey(KeyAlgorithm.ES256);
+
+            var cert = await order.Generate(new CsrInfo
+            {
+                CountryName = _configuration.Country,
+                State = _configuration.State,
+                Locality = _configuration.Locality,
+                Organization = _configuration.Organization,
+                OrganizationUnit = _configuration.Unit,
+                CommonName = _configuration.DomainName,
+            }, privateKey);
+
+            Log(" 8. Building PFX...");
+            var pfxBuilder = cert.ToPfx(privateKey);
+            var pfx = pfxBuilder.Build(_configuration.DomainName, _configuration.CertificatePassword);
+            File.WriteAllBytes(Path.Combine(_configuration.CertificatePath, $"{_configuration.DomainName}.pfx"), pfx);
+        }
+
+        private static async Task ValidateOrder(IOrderContext order)
+        {
+            Log($" 4. Validating domain {_configuration.DomainName}...");
+            var authz = (await order.Authorizations()).First();
+            var httpChallenge = await authz.Http();
+            var keyAuthz = httpChallenge.KeyAuthz;
+
+            Log(" 5. Writing challenge file");
+            var tokens = keyAuthz.Split('.');
+            await File.WriteAllTextAsync(Path.Combine(_configuration.ChallengePath, tokens[0]), keyAuthz);
+
+            var chall = await httpChallenge.Validate();
+
+            while (chall.Status == ChallengeStatus.Pending)
+            {
+                await Task.Delay(10000);
+                chall = await httpChallenge.Validate();
+            }
+
+            if (chall.Status == ChallengeStatus.Valid)
+            {
+                Log($" 6. Domain {_configuration.DomainName} is valid!");
+            }
+
+            if (chall.Status == ChallengeStatus.Invalid)
+            {
+                Log($" 6. Domain {_configuration.DomainName} is NOT valid! {chall.Error.Detail}");
+            }
+        }
+
+        private static async Task<IOrderContext> CreateOrder(AcmeContext acme)
+        {
+            Log($" 3. Creating order { _configuration.DomainName}...");
+            return await acme.NewOrder(new[] { _configuration.DomainName });
+        }
+
+        private static async Task<AcmeContext> LoadAccount()
+        {
+            AcmeContext acme;
+
+            var server = _configuration.IsStaging ? WellKnownServers.LetsEncryptStagingV2 : WellKnownServers.LetsEncryptV2;
+            Log($" 1. Setting Environment {server}...");
+
+            if (String.IsNullOrEmpty(_configuration.AccountPem))
+            {
+                Log(" 2. Creating account...");
+                acme = new AcmeContext(server);
+                var account = await acme.NewAccount(_configuration.AccountEmail, true);
+                _configuration.AccountPem = acme.AccountKey.ToPem();
+            }
+            else
+            {
+                Log(" 2. Using existing account...");
+                var accountKey = KeyFactory.FromPem(_configuration.AccountPem);
+                acme = new AcmeContext(server, accountKey);
+            }
+
+            return acme;
+        }
+
+        private static void ShowTitle()
+        {
+            Log("Let's Encrypt Client is starting...");
+            WriteLine();
+        }
+
+        private static void LoadConfiguration()
+        {
+            Log(" 0. Loading environment variables...");
+
+            var staging = Environment.GetEnvironmentVariable("STAGING");
+            var accountName = Environment.GetEnvironmentVariable("ACCOUNT_EMAIL");
+            var accountKey = Environment.GetEnvironmentVariable("ACCOUNT_KEY");
+            var domain = Environment.GetEnvironmentVariable("DOMAIN");
+            var country = Environment.GetEnvironmentVariable("CERT_COUNTRY");
+            var state = Environment.GetEnvironmentVariable("CERT_STATE");
+            var locality = Environment.GetEnvironmentVariable("CERT_LOCALITY");
+            var organization = Environment.GetEnvironmentVariable("CERT_ORGANISATION");
+            var organizationUnit = Environment.GetEnvironmentVariable("CERT_ORGANISATION_UNIT");
+            var certificatePassword = Environment.GetEnvironmentVariable("CERTIFICATE_PASSWORD");
+            var certificatePath = Environment.GetEnvironmentVariable("CERTIFICATE_PATH");
+            var challengePath = Environment.GetEnvironmentVariable("CHALLENGE_PATH");
+            var delay = Environment.GetEnvironmentVariable("DELAY");
+
+            _configuration.IsStaging = staging == "YES";
+            _configuration.AccountEmail = accountName;
+            _configuration.AccountPem = accountKey;
+            _configuration.DomainName = domain;
+            _configuration.Country = country;
+            _configuration.State = state;
+            _configuration.Locality = locality;
+            _configuration.Organization = organization;
+            _configuration.Unit = organizationUnit;
+            _configuration.Delay = Convert.ToInt32(delay);
+            _configuration.CertificatePassword = certificatePassword;
+            _configuration.CertificatePath = certificatePath;
+            _configuration.ChallengePath = challengePath;
+        }
+
+        private static void Log(String message)
+        {
+            WriteLine($"[{DateTime.Now:dd-MM-yyyy HH:mm}] {message}");
+        }
+    }
+}
